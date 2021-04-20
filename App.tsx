2cbce947f93bad276958470951572472e06922c9
Gabriel Felix <gitfelix0524@gmail.com>
2021-04-20 16:49:29 -0300

    expo-app-loading instalado

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import { View} from 'react-native'
+import AppLoading from 'expo-app-loading'
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 import Welcome from './src/pages/Welcome'
 
 export default function App(){
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return(
     <View>
       <Welcome/>
