984dec2ddce4298aac2f9ec977161579d6d2fb5b
Gabriel Felix <gitfelix0524@gmail.com>
2021-04-20 19:51:11 -0300

    TouchableWithoutFeedback para fechar o teclado ao clicar em qualquer canto da tela.

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -7,7 +7,9 @@ import {
     Text,
     TextInput,
     KeyboardAvoidingView,
-    Platform
+    Platform,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native'
 
 import Button from '../components/Button'
@@ -45,37 +47,39 @@ export default function UserIdentification(){
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
 
-                        <Text style={styles.emoji}>
-                            {isFilled ? '😄' : '😃'}
-                        </Text>
-                        
-                        <Text style={styles.title}>
-                            Como podemos{'\n'}
-                            chamar você?
-                        </Text>
-                        
-                        <TextInput
-                        style={[
-                            styles.input,
-                            (isFocused || isFilled) && 
-                            {borderColor: colors.green}
-                        ]}
-                        placeholder="Digite um nome"
-                        onBlur={handleInputBlur}
-                        onFocus={handleInputFocus}
-                        onChangeText={handleInputChange}
-                        />
+                            <Text style={styles.emoji}>
+                                {isFilled ? '😄' : '😃'}
+                            </Text>
+                            
+                            <Text style={styles.title}>
+                                Como podemos{'\n'}
+                                chamar você?
+                            </Text>
+                            
+                            <TextInput
+                            style={[
+                                styles.input,
+                                (isFocused || isFilled) && 
+                                {borderColor: colors.green}
+                            ]}
+                            placeholder="Digite um nome"
+                            onBlur={handleInputBlur}
+                            onFocus={handleInputFocus}
+                            onChangeText={handleInputChange}
+                            />
 
-                        <View style={styles.footer}>
-                            <Button 
-                                title="Confirmar"
-                                onPress={handleSubmit}/>    
+                            <View style={styles.footer}>
+                                <Button 
+                                    title="Confirmar"
+                                    onPress={handleSubmit}/>    
+                            </View>
                         </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
