60a2e81f660f6b29fbe492acaf99a7ed90f423aa
Gabriel Felix <gitfelix0524@gmail.com>
2021-04-20 16:38:38 -0300

    Expo icons instalado

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { SafeAreaView, Text, Image, StyleSheet} from 'react-native'
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
 import colors from '../../styles/colors'
-import Button from '../components/button'
+import { Feather } from '@expo/vector-icons'
 
 import watering from '../assets/watering.png'
 
@@ -10,11 +10,11 @@ export default function Welcome(){
     <SafeAreaView style={styles.Container}>
         <Text style={styles.title}>
             Gerencie{'\n'}
-            suas plantas{'\n'}
-            de forma fácil
+            suas plantas de{'\n'}
+            forma fácil
         </Text>
 
-        <Image source={watering} style={styles.image}/>
+        <Image source={watering} style={styles.image} resizeMode="contain"/>
 
         <Text style={styles.subtitle}>
             Não esqueça mais de regar suas plantas. 
@@ -22,7 +22,9 @@ export default function Welcome(){
             sempre que precisar.
         </Text>
 
-        <Button/>
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+            <Feather name="chevron-right" style={styles.buttonIcon}/>
+        </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -39,19 +41,30 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: colors.heading,
         marginTop: '18%',
-        marginBottom: '10%'
+        marginBottom: '6%'
     },
     image:{
-        width: 292,
-        height: 284,
-        marginBottom: '10%'
+        //anotar
+        height: Dimensions.get('window').width * 0.7,
     },
     subtitle:{
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
         color: colors.heading,
-        marginBottom: '10%'
+        marginBottom: '5%'
     },
-    
+    button:{
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonIcon:{
+        color: colors.white,
+        fontSize: 32
+    }
 })
